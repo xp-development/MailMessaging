cb2e7c6c283ed4384bc27276d8843609b1f75116
jan-schubert <dev@schubert-jan.de>
2014-12-11 21:12:24 +0100

    Added missing connect tests.

--- MailMessaging.Plain.WinRT.IntegrationTest/_MailMessenger/Connect.cs
+++ MailMessaging.Plain.WinRT.IntegrationTest/_MailMessenger/Connect.cs
@@ -1,12 +1,11 @@
 ﻿using FluentAssertions;
 using MailMessaging.Plain.Core;
+using NUnit.Framework;
 #if WinRT
  using MailMessaging.Plain.WinRT;
 #elif NET
  using MailMessaging.Plain.Net;
 #endif
-using NUnit.Framework;
-
 
 namespace MailMessaging.Plain.IntegrationTest._MailMessenger
 {
@@ -21,6 +20,26 @@ namespace MailMessaging.Plain.IntegrationTest._MailMessenger
             var tcpClient = new TcpClient();
             var messenger = new MailMessenger(account, tcpClient);
             messenger.Connect().Result.Should().Be(ConnectResult.Connected);
+        }
+
+        [Test]
+        public void ShouldNotConnectServerIfHostIsWrong()
+        {
+            var account = new Account("222.222.222.222", 51234, "validUserName", "validPassword", false);
+
+            var tcpClient = new TcpClient();
+            var messenger = new MailMessenger(account, tcpClient);
+            messenger.Connect().Result.Should().Be(ConnectResult.UnknownHost);
+        }
+
+        [Test]
+        public void ShouldNotConnectServerIfPortIsWrong()
+        {
+            var account = new Account("127.0.0.1", 11111, "validUserName", "validPassword", false);
+
+            var tcpClient = new TcpClient();
+            var messenger = new MailMessenger(account, tcpClient);
+            messenger.Connect().Result.Should().Be(ConnectResult.UnknownHost);
         }
     }
 }
